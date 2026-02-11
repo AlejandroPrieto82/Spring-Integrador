b71dbb21ec3b281a38f97a63730e49c8f8ec5bcc
Alejandro Prieto <samuel.prieto-r@mail.escuelaing.edu.co>
2026-02-11 13:41:41 -0500

    spring intialicer

---
+++ src/main/java/eci/edu/healt/HealtApplication.java
@@ -0,0 +1,13 @@
+package eci.edu.healt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eci/edu/healt/HealtApplicationTests.java
@@ -0,0 +1,13 @@
+package eci.edu.healt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
